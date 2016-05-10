Fix rotation matrix

main.py
@@ -167,18 +167,18 @@ def rotation_matrix_from_euler(psi, theta, phi):
     return np.asarray(
         [
             [
-                np.cos(phi) * np.cos(psi) - np.cos(theta) * np.sin(phi) * np.cos(psi),
-                np.cos(theta) * np.cos(psi) * np.sin(phi) + np.cos(phi) * np.sin(psi),
-                np.sin(theta) * np.cos(phi)
+                np.cos(phi) * np.cos(psi) - np.cos(theta) * np.sin(phi) * np.sin(psi),
+                -np.cos(theta) * np.sin(psi) * np.cos(phi) - np.cos(phi) * np.sin(psi),
+                np.sin(theta) * np.sin(psi)
             ],
             [
-                -np.cos(psi) * np.sin(phi) - np.cos(theta) * np.cos(phi) * np.sin(psi),
+                np.sin(psi) * np.cos(phi) + np.cos(theta) * np.cos(psi) * np.sin(phi),
                 np.cos(theta) * np.cos(phi) * np.cos(psi) - np.sin(phi) * np.sin(psi),
-                np.cos(phi) * np.sin(theta)
+                -np.cos(phi) * np.sin(theta)
             ],
             [
                 np.sin(theta) * np.sin(psi),
-                -np.cos(psi) * np.sin(theta),
+                np.cos(phi) * np.sin(theta),
                 np.cos(theta)
             ]
         ]