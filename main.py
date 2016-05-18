6b76e92f7b1c0678e2d9b699fed3ed0d9bdbb8c4
Eugene Aslanov <aslanov.ev@gmail.com>
2016-05-18 13:30:30 +0400

    Add Belecki stable  points and presentation

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ import scipy
 from scipy.constants import G
 from astropy.constants import M_earth, R_earth
 import math
+from astropy.units.si import sday
+
+omega_earth = 2 * np.pi / sday.si.scale
 
 M_e = M_earth.value
 mu = G * M_e
@@ -237,7 +240,7 @@ def rotation_matrix_from_euler(psi, theta, phi):
 parameters = Parameters(open("sample.json"))
 vy0 = 8000
 x0 = R + parameters.h
-q_0 = np.array([x0, 0, 0, vy0, 0, 0, 0, 0, np.pi / 6, 0])
+q_11_0 = np.array([x0, 0, 0, vy0, 0, 0, 0, 0, np.pi / 6, 0])
 tk = parameters.T * 128
 
 k = G * (parameters.m + M_e)
@@ -251,33 +254,33 @@ r = p / (1 + e * np.cos(nu))
 x = r * np.cos(nu)
 y = r * np.sin(nu)
 
-solver = ode(dq).set_integrator('dopri5', nsteps=1)
-solver.set_initial_value(q_0, 0).set_f_params(parameters)
+solver_11 = ode(dq).set_integrator('dopri5', nsteps=1)
+solver_11.set_initial_value(q_11_0, 0).set_f_params(parameters)
 
-sol_t = []
-sol_q = []
-while solver.t < tk:
-    solver.integrate(tk, step=True)
-    sol_t.append(solver.t)
-    sol_q.append(solver.y)
+sol_t_11 = []
+sol_q_11 = []
+while solver_11.t < tk:
+    solver_11.integrate(tk, step=True)
+    sol_t_11.append(solver_11.t)
+    sol_q_11.append(solver_11.y)
 
-sol_t = np.array(sol_t).reshape((len(sol_t), 1))
-sol_q = np.array(sol_q)
+sol_t_11 = np.array(sol_t_11).reshape((len(sol_t_11), 1))
+sol_q_11 = np.array(sol_q_11)
 
-rx = sol_q[:, 0]
-ry = sol_q[:, 1]
+rx = sol_q_11[:, 0]
+ry = sol_q_11[:, 1]
 
-plt.plot(sol_t, rx, label="rx")
-plt.plot(sol_t, ry, label="ry")
+plt.plot(sol_t_11, rx, label="rx")
+plt.plot(sol_t_11, ry, label="ry")
 plt.grid()
 plt.legend()
 
-vx = sol_q[:, 2]
-vy = sol_q[:, 3]
+vx = sol_q_11[:, 2]
+vy = sol_q_11[:, 3]
 
 plt.figure()
-plt.plot(sol_t, vx, label="vx")
-plt.plot(sol_t, vy, label="vy")
+plt.plot(sol_t_11, vx, label="vx")
+plt.plot(sol_t_11, vy, label="vy")
 plt.grid()
 plt.legend()
 
@@ -287,28 +290,332 @@ plt.plot(x, y, label="analytic")
 plt.grid()
 plt.legend()
 
-wx = sol_q[:, 4]
-wy = sol_q[:, 5]
-wz = sol_q[:, 6]
+wx = sol_q_11[:, 4]
+wy = sol_q_11[:, 5]
+wz = sol_q_11[:, 6]
 
 plt.figure()
-plt.plot(sol_t, wx, label='w_x')
-plt.plot(sol_t, wy, label='w_y')
-plt.plot(sol_t, wz, label='w_z')
+plt.plot(sol_t_11, wx, label='w_x')
+plt.plot(sol_t_11, wy, label='w_y')
+plt.plot(sol_t_11, wz, label='w_z')
 plt.grid()
 plt.legend()
 
-psi = sol_q[:, 7]
-theta = sol_q[:, 8]
-phi = sol_q[:, 9]
+psi_11 = sol_q_11[:, 7]
+theta_11 = sol_q_11[:, 8]
+phi = sol_q_11[:, 9]
 plt.figure()
-plt.plot(sol_t, psi, label="psi")
-plt.plot(sol_t, theta, label="theta")
-plt.plot(sol_t, phi, label="phi")
+plt.plot(sol_t_11, psi_11, label="psi")
+plt.plot(sol_t_11, theta_11, label="theta")
+plt.plot(sol_t_11, phi, label="phi")
 plt.grid()
 plt.legend()
 
-plt.show()
+
+def dr(t, q, parameters: Parameters, r0: float, omega_0: float):
+    beta = r0 / omega_0
+    J = parameters.J
+    alpha = J[2] / J[0]
+    p = q[0: 2]
+    angle = q[2: 4]
+
+    dp = np.zeros(2)
+    dp[0] = (p[0] / np.tan(angle[1]) - alpha * beta / (np.sin(angle[1]))) * np.sin(angle[0]) - p[1] * np.cos(angle[0])
+    dp[1] = - (p[0] ** 2) / (np.sin(angle[1]) ** 3) * np.cos(angle[1]) \
+            + p[0] / (np.sin(angle[1]) ** 2) * np.cos(angle[0]) \
+            + alpha * beta * p[0] * (1 + np.cos(angle[1]) ** 2) / (np.sin(angle[1]) ** 3) \
+            - alpha * beta * (np.cos(angle[0])) / (np.sin(angle[1]) ** 2) * np.cos(angle[1]) \
+            - alpha ** 2 * beta ** 2 / (np.tan(angle[1]) * (np.sin(angle[1]) ** 2)) \
+            - 3 * (alpha - 1) * np.cos(angle[1]) * np.sin(angle[1])
+    dangle = np.zeros(2)
+    dangle[0] = p[0] / (np.sin(angle[1]) ** 2) - np.cos(angle[0]) / np.tan(angle[1]) \
+                - alpha * beta * np.cos(angle[1]) / (np.sin(angle[1]) ** 2)
+    dangle[1] = p[1] - np.sin(angle[0])
+
+    result = dp
+    result = np.append(result, dangle)
+    return result
+
+
+J = parameters.J
+alpha = J[2] / J[0]
+omega_0 = np.sqrt(mu / (R ** 3))
+r0 = 0.002
+print(r0)
+print(mu / (R ** 3))
+print(omega_0)
+beta = r0 / omega_0
+print(beta)
+print(alpha)
+print(alpha * beta)
+print(omega_0 / alpha)
+cos_psi_11_0 = - alpha * beta
+p_theta_11_0 = np.sqrt(1 - cos_psi_11_0 ** 2)
+theta_11_0 = np.pi / 2
+p_psi_11_0 = 0
+q_11_0 = np.array([p_psi_11_0, p_theta_11_0, np.arccos(cos_psi_11_0), theta_11_0])
+cos_psi_12_0 = - alpha * beta
+p_theta_12_0 = np.sqrt(1 - cos_psi_12_0 ** 2)
+theta_12_0 = np.pi / 2 + 0.01
+p_psi_12_0 = 0
+q_12_0 = np.array([p_psi_12_0, p_theta_12_0, np.arccos(cos_psi_12_0), theta_12_0])
+cos_psi_13_0 = - alpha * beta
+p_theta_13_0 = np.sqrt(1 - cos_psi_13_0 ** 2)
+theta_13_0 = np.pi / 2 + 0.02
+p_psi_13_0 = 0
+q_13_0 = np.array([p_psi_13_0, p_theta_13_0, np.arccos(cos_psi_13_0), theta_13_0])
+theta_21_0 = np.pi / 2
+sin_psi_21_0 = 0
+p_theta_21_0 = 0
+p_psi_21_0 = 0
+q_21_0 = np.array([p_psi_21_0, p_theta_21_0, np.arcsin(sin_psi_21_0), theta_21_0])
+theta_22_0 = np.pi / 2 + 0.01
+sin_psi_22_0 = 0
+p_theta_22_0 = 0
+p_psi_22_0 = 0
+q_22_0 = np.array([p_psi_22_0, p_theta_22_0, np.arcsin(sin_psi_22_0), theta_22_0])
+theta_23_0 = np.pi / 2 + 0.02
+sin_psi_23_0 = 0
+p_theta_23_0 = 0
+p_psi_23_0 = 0
+q_23_0 = np.array([p_psi_23_0, p_theta_23_0, np.arcsin(sin_psi_23_0), theta_23_0])
+psi_31_0 = 0
+p_theta_31_0 = 0
+sin_theta_31_0 = alpha * beta / (3. * alpha - 4.)
+p_psi_31_0 = 3. * (alpha - 1.) * sin_theta_31_0 * np.sqrt(1. - sin_theta_31_0 ** 2)
+print(sin_theta_31_0)
+print(p_psi_31_0)
+q_31_0 = np.array([p_psi_31_0, p_theta_31_0, psi_31_0, np.arcsin(sin_theta_31_0)])
+psi_32_0 = 0 + 0.01
+p_theta_32_0 = 0
+sin_theta_32_0 = alpha * beta / (3. * alpha - 4.)
+p_psi_32_0 = 3. * (alpha - 1.) * sin_theta_32_0 * np.sqrt(1. - sin_theta_32_0 ** 2)
+q_32_0 = np.array([p_psi_32_0, p_theta_32_0, psi_32_0, np.arcsin(sin_theta_32_0)])
+psi_33_0 = 0 + 0.1
+p_theta_33_0 = 0
+sin_theta_33_0 = alpha * beta / (3. * alpha - 4.)
+p_psi_33_0 = 3. * (alpha - 1.) * sin_theta_33_0 * np.sqrt(1. - sin_theta_33_0 ** 2)
+q_33_0 = np.array([p_psi_33_0, p_theta_33_0, psi_33_0, np.arcsin(sin_theta_33_0)])
+tk = 30
+
+solver_11 = ode(dr).set_integrator('dopri5', nsteps=1)
+solver_11.set_initial_value(q_11_0, 0).set_f_params(parameters, r0, omega_0)
+solver_12 = ode(dr).set_integrator('dopri5', nsteps=1)
+solver_12.set_initial_value(q_12_0, 0).set_f_params(parameters, r0, omega_0)
+solver_13 = ode(dr).set_integrator('dopri5', nsteps=1)
+solver_13.set_initial_value(q_13_0, 0).set_f_params(parameters, r0, omega_0)
+solver_21 = ode(dr).set_integrator('dopri5', nsteps=1)
+solver_21.set_initial_value(q_21_0, 0).set_f_params(parameters, r0, omega_0)
+solver_22 = ode(dr).set_integrator('dopri5', nsteps=1)
+solver_22.set_initial_value(q_22_0, 0).set_f_params(parameters, r0, omega_0)
+solver_23 = ode(dr).set_integrator('dopri5', nsteps=1)
+solver_23.set_initial_value(q_23_0, 0).set_f_params(parameters, r0, omega_0)
+solver_31 = ode(dr).set_integrator('dopri5', nsteps=1)
+solver_31.set_initial_value(q_31_0, 0).set_f_params(parameters, r0, omega_0)
+solver_32 = ode(dr).set_integrator('dopri5', nsteps=1)
+solver_32.set_initial_value(q_32_0, 0).set_f_params(parameters, r0, omega_0)
+solver_33 = ode(dr).set_integrator('dopri5', nsteps=1)
+solver_33.set_initial_value(q_33_0, 0).set_f_params(parameters, r0, omega_0)
+
+sol_t_11 = []
+sol_q_11 = []
+sol_t_12 = []
+sol_q_12 = []
+sol_t_13 = []
+sol_q_13 = []
+sol_t_21 = []
+sol_q_21 = []
+sol_t_22 = []
+sol_q_22 = []
+sol_t_23 = []
+sol_q_23 = []
+sol_t_31 = []
+sol_q_31 = []
+sol_t_32 = []
+sol_q_32 = []
+sol_t_33 = []
+sol_q_33 = []
+while solver_11.t < tk:
+    solver_11.integrate(tk, step=True)
+    sol_t_11.append(solver_11.t)
+    sol_q_11.append(solver_11.y)
+while solver_12.t < tk:
+    solver_12.integrate(tk, step=True)
+    sol_t_12.append(solver_12.t)
+    sol_q_12.append(solver_12.y)
+while solver_13.t < tk:
+    solver_13.integrate(tk, step=True)
+    sol_t_13.append(solver_13.t)
+    sol_q_13.append(solver_13.y)
+while solver_21.t < tk:
+    solver_21.integrate(tk, step=True)
+    sol_t_21.append(solver_21.t)
+    sol_q_21.append(solver_21.y)
+while solver_22.t < tk:
+    solver_22.integrate(tk, step=True)
+    sol_t_22.append(solver_22.t)
+    sol_q_22.append(solver_22.y)
+while solver_23.t < tk:
+    solver_23.integrate(tk, step=True)
+    sol_t_23.append(solver_23.t)
+    sol_q_23.append(solver_23.y)
+tk = 1.58
+while solver_31.t < tk:
+    solver_31.integrate(tk, step=True)
+    sol_t_31.append(solver_31.t)
+    sol_q_31.append(solver_31.y)
+while solver_32.t < tk:
+    solver_32.integrate(tk, step=True)
+    sol_t_32.append(solver_32.t)
+    sol_q_32.append(solver_32.y)
+while solver_33.t < tk:
+    solver_33.integrate(tk, step=True)
+    sol_t_33.append(solver_33.t)
+    sol_q_33.append(solver_33.y)
+
+sol_t_11 = np.array(sol_t_11).reshape((len(sol_t_11), 1))
+sol_q_11 = np.array(sol_q_11)
+
+p_psi_11 = sol_q_11[:, 0]
+p_theta_11 = sol_q_11[:, 1]
+psi_11 = sol_q_11[:, 2]
+theta_11 = sol_q_11[:, 3]
+
+sol_t_12 = np.array(sol_t_12).reshape((len(sol_t_12), 1))
+sol_q_12 = np.array(sol_q_12)
+
+p_psi_12 = sol_q_12[:, 0]
+p_theta_12 = sol_q_12[:, 1]
+psi_12 = sol_q_12[:, 2]
+theta_12 = sol_q_12[:, 3]
+
+sol_t_13 = np.array(sol_t_13).reshape((len(sol_t_13), 1))
+sol_q_13 = np.array(sol_q_13)
+
+p_psi_13 = sol_q_13[:, 0]
+p_theta_13 = sol_q_13[:, 1]
+psi_13 = sol_q_13[:, 2]
+theta_13 = sol_q_13[:, 3]
+
+sol_t_21 = np.array(sol_t_21).reshape((len(sol_t_21), 1))
+sol_q_21 = np.array(sol_q_21)
+
+p_psi_21 = sol_q_21[:, 0]
+p_theta_21 = sol_q_21[:, 1]
+psi_21 = sol_q_21[:, 2]
+theta_21 = sol_q_21[:, 3]
+
+sol_t_22 = np.array(sol_t_22).reshape((len(sol_t_22), 1))
+sol_q_22 = np.array(sol_q_22)
+
+p_psi_22 = sol_q_22[:, 0]
+p_theta_22 = sol_q_22[:, 1]
+psi_22 = sol_q_22[:, 2]
+theta_22 = sol_q_22[:, 3]
+
+sol_t_23 = np.array(sol_t_23).reshape((len(sol_t_23), 1))
+sol_q_23 = np.array(sol_q_23)
+
+p_psi_23 = sol_q_23[:, 0]
+p_theta_23 = sol_q_23[:, 1]
+psi_23 = sol_q_23[:, 2]
+theta_23 = sol_q_23[:, 3]
+
+sol_t_31 = np.array(sol_t_31).reshape((len(sol_t_31), 1))
+sol_q_31 = np.array(sol_q_31)
+
+p_psi_31 = sol_q_31[:, 0]
+p_theta_31 = sol_q_31[:, 1]
+psi_31 = sol_q_31[:, 2]
+theta_31 = sol_q_31[:, 3]
+
+sol_t_32 = np.array(sol_t_32).reshape((len(sol_t_32), 1))
+sol_q_32 = np.array(sol_q_32)
+
+p_psi_32 = sol_q_32[:, 0]
+p_theta_32 = sol_q_32[:, 1]
+psi_32 = sol_q_32[:, 2]
+theta_32 = sol_q_32[:, 3]
+
+sol_t_33 = np.array(sol_t_33).reshape((len(sol_t_33), 1))
+sol_q_33 = np.array(sol_q_33)
+
+p_psi_33 = sol_q_33[:, 0]
+p_theta_33 = sol_q_33[:, 1]
+psi_33 = sol_q_33[:, 2]
+theta_33 = sol_q_33[:, 3]
+
+import os
+directory = "images"
+if not os.path.exists(directory):
+    os.mkdir("images")
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.plot(theta_11, p_theta_11, "+b", label="delta theta = 0")
+ax.plot(theta_12, p_theta_12, "g", label="delta theta = 0.01")
+ax.plot(theta_13, p_theta_13, "r", label="delta theta = 0.02")
+ax.grid()
+ax.legend()
+ax.set_xlabel("theta")
+ax.set_ylabel("p_theta")
+fig.savefig("images/stat_1_theta.png")
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.plot(psi_11, p_psi_11, "+b", label="delta theta = 0")
+ax.plot(psi_12, p_psi_12, "g", label="delta theta = 0.01")
+ax.plot(psi_13, p_psi_13, "r", label="delta theta = 0.02")
+ax.grid()
+ax.legend()
+ax.set_xlabel("psi")
+ax.set_ylabel("p_psi")
+fig.savefig("images/stat_1_psi.png")
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.plot(theta_21, p_theta_21, "+b", label="delta theta = 0")
+ax.plot(theta_22, p_theta_22, "g", label="delta theta = 0.01")
+ax.plot(theta_23, p_theta_23, "r", label="delta theta = 0.02")
+ax.grid()
+ax.legend()
+ax.set_xlabel("theta")
+ax.set_ylabel("p_theta")
+fig.savefig("images/stat_2_theta.png")
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.plot(psi_21, p_psi_21, "+b", label="delta theta = 0")
+ax.plot(psi_22, p_psi_22, "g", label="delta theta = 0.01")
+ax.plot(psi_23, p_psi_23, "r", label="delta theta = 0.02")
+ax.grid()
+ax.legend()
+ax.set_xlabel("psi")
+ax.set_ylabel("p_psi")
+fig.savefig("images/stat_2_psi.png")
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.plot(theta_31, p_theta_31, "+b", label="delta theta = 0")
+ax.plot(theta_32, p_theta_32, "g", label="delta theta = 0.01")
+ax.plot(theta_33, p_theta_33, "r", label="delta theta = 0.02")
+ax.grid()
+ax.legend(loc='upper left')
+ax.set_xlabel("theta")
+ax.set_ylabel("p_theta")
+fig.savefig("images/stat_3_theta.png")
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.plot(psi_31, p_psi_31, "+b", label="delta theta = 0")
+ax.plot(psi_32, p_psi_32, "g", label="delta theta = 0.01")
+ax.plot(psi_33, p_psi_33, "r", label="delta theta = 0.02")
+ax.grid()
+ax.legend(loc='upper left')
+ax.set_xlabel("psi")
+ax.set_ylabel("p_psi")
+fig.savefig("images/stat_3_psi.png")
+
+# plt.show()
 
 # def p(parameters: Parameters, t: float) -> float:
 #     p_ = parameters.p
